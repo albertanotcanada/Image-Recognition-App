a66c1b9fbfd327618656b4974bda7efe2cf76a0c
Amy Winkler <amycwinkler@gmail.com>
2018-01-06 21:42:03 -0500

    i added the camera i think

--- App.js
+++ App.js
@@ -6,20 +6,28 @@ import Inputs from './screens/Inputs.js';
 import Camera from 'react-native-camera';
 
 export default class App extends React.Component {
+    takePicture() {
+        this.camera.capture()
+            .then((data) => console.log(data))
+            .catch(err => console.error(err));
+    }
   render() {
 
+
     return (
-
+    <View>
         <Inputs />
-  //       <Camera
-  //     ref={(cam) => {
-  //         this.camera = cam;
-  //     }}
-  //     aspect={Camera.constants.Aspect.fill}>
-  // <Text onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
-  // </Camera>
+        <Camera
+            ref={(cam) => {
+                this.camera = cam;
+            }}
+            style={styles.preview}
+            aspect={Camera.constants.Aspect.fill}>
+            <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+        </Camera>
 
 
+    </View>
 
     );
   }
